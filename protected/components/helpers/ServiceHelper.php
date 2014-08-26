21905c2502791e083ae35b2b2d908f3f70073e4e
abba <abba@workdeb>
2014-08-27 00:40:48 +0700

    fixed

---
+++ protected/components/helpers/ServiceHelper.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ServiceHelper
+ *
+ * @version 1.0.0
+ * @copyright Copyright 2011 by Kirya <cloudkserg11@gmail.com>
+ * @author Kirya <cloudkserg11@gmail.com>
+ */
+class ServiceHelper
+{
+
+    /**
+     * generatePopupData
+     *
+     * @param mixed $service
+     * @return void
+     */
+    public static function generatePopupData($service) {
+        $jsonData = '{';
+        
+        $jsonData .= '"desc":"'.$service->text.'",';
+        $jsonData .= '"fractions":[';
+        
+        $arrHelper = new ArrayHelper($service->fractions);
+        
+        foreach ($service->fractions as $key => $fraction) {
+            $jsonData .= '{"fraction":"'.$fraction->title.'", "price":"'.$fraction->price.'", "img": "' . PImageHelper::firstImage($fraction, 'alt') . '"}';
+            
+            if (!$arrHelper->isLastKey($key)) {
+                $jsonData .= ',';
+            }
+            
+        }
+        
+        $jsonData .= ']}';
+        
+        return trim(preg_replace('/\s\s+/', ' ', $jsonData));
+    }
+
+
+}
